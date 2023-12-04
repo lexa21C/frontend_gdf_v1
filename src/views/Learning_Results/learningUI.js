44c8fb8c83769030ca3b3cb356ed21d95b37b4f6
Wilman <130717547+Wiilmar@users.noreply.github.com>
2023-12-04 12:35:16 -0500

    learning

---
+++ src/views/Learning_Results/learningUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { List } from './List.js';
+
+function LearningResultUI() {
+  // In your LearningResultUI component
+  return (
+    <>
+      <List />
+    </>
+  );
+}
+
+export { LearningResultUI };
